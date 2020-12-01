59e8fdd6463a1fc612a7267afb70a67ce3f3e6ae
Louis-Young <7589523+louisyoung1@user.noreply.gitee.com>
2020-12-02 03:00:57 +0800

    fix(PWA):修复了Service-Worker.js失效问题

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue';
 import router from './router'
 import store from './store'
+import './registerServiceWorker'
 const rocket = createApp(App);
 rocket.use(store).use(router).mount('#app')
 
--- vue.config.js
+++ vue.config.js
@@ -16,10 +16,34 @@ module.exports = {
         appleMobileWebAppStatusBarStyle: 'black',
         iconPaths:{
             favicon16: "./img/icons/colors/Rocket-32x.png",
-            favicon32: "./img/icons/single/Rocket-16x.png",
+            favicon32: "./img/icons/single/Rocket-16x-full.png",
             appleTouchIcon: "./img/icons/colors/Rocket-192x.png",
             maskIcon: "./img/icons/colors/Rocket-16x.svg",
             msTileImage: "./img/icons/colors/Rocket-144x.png",
+        },
+        workboxPluginMode: 'GenerateSW', // 也可以定义为‘InjectManifest’模式。但是需自己写SW.js文件进行配置
+        workboxOptions: {
+            importWorkboxFrom: 'local', //从''cdn"导入workbox,也可以‘local’
+            skipWaiting: true, // 安装完SW不等待直接接管网站
+            clientsClaim: true,
+            /*
+            navigateFallback: '/',
+            navigateFallbackBlacklist: [/\/API\//],
+            exclude: [/\.(?:png|jpg|jpeg|svg)$/], //在预缓存中排除图片
+            // 定义运行时缓存
+            runtimeCaching: [
+                {
+                    urlPattern: new RegExp('^https://cdn'),
+                    handler: 'NetworkFirst',
+                    options: {
+                        networkTimeoutSeconds: 20,
+                        cacheName: 'cdn-cache',
+                        cacheableResponse: {
+                            statuses: [200]
+                        }
+                    }
+                }
+            ]*/
         }
     }
 }
